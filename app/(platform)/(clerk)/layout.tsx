f6a1b958699fc22fcdf34afeded433a52f6e01ab
HarryDang <danghuuphuc001@gmail.com>
2024-06-13 10:11:25 +0700

    feat(clerk auth): implemting clerk

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screen items-center justify-center">{children}</div>
+  );
+};
+
+export default ClerkLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/lib/config";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,8 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
