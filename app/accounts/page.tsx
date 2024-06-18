03517fce2f4eafdb28c9a39e9fa2d0390bf8583b
phuln0710vn <phulnse161272@fpt.edu.vn>
2024-06-18 23:51:15 +0700

    feat: update sidebar

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,20 +1,20 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import SideBar from '@/components/SideBar';
-import {Profile} from './component/profile';
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import SideBar from "@/components/SideBar";
+import { Profile } from "./component/profile";
 
 const Accounts = () => {
   return (
-    <div className='flex min-h-screen'>
+    <div className="flex min-h-screen">
       <SideBar />
-      <div className='flex flex-col flex-grow'>
-        <Header title='Accounts'/>
-        <main className='flex-grow bg-gray-100 p-6 pb-20 overflow-auto mt-16 ml-64'>
-          <Profile/>
+      <div className="flex flex-col flex-grow">
+        <Header title="Accounts" />
+        <main className="flex-grow bg-gray-100 p-6 pb-20 overflow-auto mt-16 ml-64">
+          <Profile />
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
-  )
-}
- export default Accounts;
+  );
+};
+export default Accounts;
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,45 +1,60 @@
+import {
+  CircleUserRound,
+  MessageSquareText,
+  Package,
+  ShoppingCart,
+  SquareGanttChart,
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
 export const Navigation = () => {
-    const pathname = usePathname();
-  
-    const getLinkClass = (path: string) => {
-      return pathname === path ? 'bg-pink-700' : 'hover:bg-pink-500' ;
-    };
+  const pathname = usePathname();
+
+  const getLinkClass = (path: string) => {
+    return pathname === path ? "bg-gray-400" : "hover:bg-gray-200";
+  };
   return (
     <div>
-      <aside className="w-64 bg-pink-500 text-white fixed left-0 top-0 bottom-0">
-        <div className="p-4 text-2xl font-bold border border-black h-[65px] bg-pink-600">
+      <aside className="w-64 bg-[#FCFBF4]  text-white fixed left-0 top-0 bottom-0">
+        <div className="p-4 text-2xl font-bold border border-black h-[65px] bg-[#F5F7FD] text-black shadow-lg">
           CucCung
         </div>
         <nav className="mt-6">
-          <Link href="/" className={`block p-4 ${getLinkClass("/")}`}>
+          <Link
+            href="/"
+            className={`p-4 text-black flex  ${getLinkClass("/")}`}
+          >
+            <SquareGanttChart className="mr-1" />
             Dashboard
           </Link>
           <Link
             href="/productManagement"
-            className={`block p-4 ${getLinkClass("/productManagement")}`}
+            className={`flex p-4 text-black ${getLinkClass("/productManagement")}`}
           >
+            <Package className="mr-1" />
             Product Management
           </Link>
           <Link
             href="/orders"
-            className={`block p-4 ${getLinkClass("/orders")}`}
+            className={`flex p-4 text-black ${getLinkClass("/orders")}`}
           >
+            <ShoppingCart className="mr-1" />
             Orders
           </Link>
           <Link
             href="/feedback"
-            className={`block p-4 ${getLinkClass("/feedback")}`}
+            className={`flex p-4 text-black ${getLinkClass("/feedback")}`}
           >
+            <MessageSquareText className="mr-1" />
             Feedback
           </Link>
           <Link
             href="/accounts"
-            className={`block p-4 ${getLinkClass("/accounts")}`}
+            className={`flex p-4 text-black ${getLinkClass("/accounts")}`}
           >
+            <CircleUserRound className="mr-1" />
             Accounts
           </Link>
         </nav>
