83a78709d9caa3c9c8b77e0f003d66a2cbc6ffb7
Wibu11090471 <vuquan11090471@gmail.com>
2024-07-18 09:54:49 +0700

    fix Sidebar 9h54

--- app/(platform)/(manage)/layout.tsx
+++ app/(platform)/(manage)/layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/Header";
-import { SideBar } from "@/components/SideBar";
+import { SideBar } from "@/components/Sidebar";
 import React from "react";
 
 interface LayoutProps {
